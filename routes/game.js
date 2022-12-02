2223c3dccb5a85c0012c01da0d306af13d3d8b8e
rtmoffat <rtmoffat@gmail.com>
2022-12-02 00:16:30 -0500

    Added player list. Need to fix pug iterator

--- routes/game.js
+++ routes/game.js
@@ -8,8 +8,17 @@ require('dotenv').config()
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DB_DATABASE,connectionLimit: 5});
 
-router.get('/',(req,res,next) => {
-    res.render('info',{username:req.cookies.username});
+router.get('/:gameid?',(req,res,next) => {
+    //players(names)
+    //feeds(ids)
+    var q='select userid,username from players join users on players.userid=users.id where gameid=1;';
+    querydb(q)
+        .then((value) => {res.render('game',{username:req.cookies.username,gameid:req.params.gameid,res:value,lres:value.length});},
+        (error) => { console.log(error); });
 })
+
+async function querydb(myq) {
+    return await pool.query({rowsAsArray: false,sql:myq});
+}
 //router.post("/setCookie", (req, res) => {
 module.exports = router;
