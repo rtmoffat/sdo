5b8432c80374d5c0d5202e0f1799f70206b604b1
rtmoffat <rtmoffat@gmail.com>
2022-12-06 10:27:14 -0500

    Added port to DB connection

--- routes/game.js
+++ routes/game.js
@@ -6,7 +6,7 @@ var md5 = require('md5');
 
 require('dotenv').config()
 const mariadb = require('mariadb');
-const pool = mariadb.createPool({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DB_DATABASE,connectionLimit: 5});
+const pool = mariadb.createPool({host: process.env.DB_HOST, port: process.env.DB_PORT,user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DB_DATABASE,connectionLimit: 5});
 
 router.get('/:gameid?',(req,res,next) => {
     //players(names)
