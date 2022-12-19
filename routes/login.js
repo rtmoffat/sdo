f75ffcdbe367c6a2238e499048ee7ede7bdec2a1
rtmoffat <rtmoffat@gmail.com>
2022-12-19 13:02:30 -0500

    Cleaned up login process

--- routes/login.js
+++ routes/login.js
@@ -26,54 +26,51 @@ router.get('/', function(req, res, next) {
 
 router.post("/setCookie", (req, res) => {
     console.log('running setcookie');
-    //Check if a token is already set. If so, we don't need to set it again
-    //Check username/password
-    let q='select id from users where username=(?) and password=(?)';
-    //let v=[req.headers.username,req.headers.password+process.env.SALT];
-    let v=[req.headers.username,md5(req.headers.password+process.env.SALT)];
-    console.log(md5(req.headers.password+process.env.SALT));
-    queryDb(q,v)
-      .then((value) => {
-        console.log(value);
-        console.log(req.headers.username);
-        if (value.length==0) {
-          console.log("Username/password invalid");
-          res.send('Username/password invalid!!!');
-        }
-        else {
-          console.log('login successful!');
-          //res.send('login successful!!!');
-          let newToken=md5(req.headers.username+process.env.SALT);
-          let q='UPDATE users set apikey=(?) WHERE username=(?)';
-          let v=[newToken,req.headers.username];
-          //let q='UPDATE sdo WHERE username=='+req.headers.username+' set apikey='+newToken+';';
-          queryDb(q,v)
-            .then ((value) => {
-            res
-              .cookie('token',newToken,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
-              .cookie('username',req.headers.username,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
-              .send('login successful!');
-            });
-        }
-      });
-    /*console.log(req.cookies.username);
+    //Check if a token is already set.
+    //Check that the token is valid for the username
     if (req.cookies.username && req.cookies.token) { 
       console.log(req.cookies.username+' already has a token of '+req.cookies.token);
-    }
-    else {
-      let newToken=md5(req.headers.username+process.env.SALT);
-      let q='UPDATE users set apikey=(?) WHERE username=(?)';
-      let v=[newToken,req.headers.username];
-      //let q='UPDATE sdo WHERE username=='+req.headers.username+' set apikey='+newToken+';';
+      let q='select id from users where username=(?) and apikey=(?)';
+      let v=[req.cookies.username,req.cookies.token];
       queryDb(q,v)
-        .then ((value) => {
-        res
-          .cookie('token',newToken,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
-          .cookie('username',req.headers.username,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
-          .send('login successful!');
-        })
-    }*/
-});
+        .then((value) => {
+          if (value.length==1) {
+            console.log('Welcome back!');
+            res.send('Welcome back!!');
+          }
+        });
+      }
+      else {
+        //Verify username/password
+        let q='select id from users where username=(?) and password=(?)';
+        let v=[req.headers.username,md5(req.headers.password+process.env.SALT)];
+        console.log(md5(req.headers.password+process.env.SALT));
+        queryDb(q,v)
+          .then((value) => {
+            console.log(value);
+            console.log(req.headers.username);
+            if (value.length==0) {
+              console.log("Username/password invalid");
+              res.send('Username/password invalid!!!');
+            }
+            //Set the token
+            else {
+              console.log('login successful!! Welcome to SDO!');
+              let newToken=md5(req.headers.username+process.env.SALT);
+              let q='UPDATE users set apikey=(?) WHERE username=(?)';
+              let v=[newToken,req.headers.username];
+              queryDb(q,v)
+                .then ((value) => {
+                res
+                  .cookie('token',newToken,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
+                  .cookie('username',req.headers.username,{httpOnly:true,sameSite:'lax',maxAge:9000000000,path:'/'})
+                  .send('login successful!');
+                });
+            }
+          });
+      }
+    }
+);
   router.get("/private", (req, res) => {
     if (!req.cookies.token) return res.status(401).send();
     res.status(200).json({ secret: "Ginger ale is a specific Root Beer" });
